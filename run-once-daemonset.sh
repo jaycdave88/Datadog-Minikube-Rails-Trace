367f62a61e65668b6b316cdde5c988da3b70330d
jay.dave <jay.dave@COMP10172.attlocal.net>
2018-08-23 13:40:32 -0500

    updated the shell script

--- run-once-daemonset.sh
+++ run-once-daemonset.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 function main() {
   kubectl apply -f datadog-agent.yaml
+  create_database
   apply_api_key
 }
 
@@ -13,5 +14,9 @@ function apply_api_key() {
   DD_API_KEY={API_KEY} docker-compose up
 
   echo
+}
+function create_database(){
+	docker-compose run --rm web rake db:create
+	docker-compose run --rm web rake db:migrate
 }
 main
